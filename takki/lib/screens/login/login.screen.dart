037623ab0e97909c46ff3be9accf1f6985d832ef
Ronel Cruz <ronel.cruz.a8@gmail.com>
2025-07-09 23:40:48 -0400

    Update login.screen.dart

--- takki/lib/screens/login/login.screen.dart
+++ takki/lib/screens/login/login.screen.dart
@@ -14,39 +14,41 @@ class LoginScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Takki',style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold,color: Color(0xFF818181)),),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Takki',style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold,color: Color(0xFF818181)),),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: TextfielWidget(label: 'Correo electronico',controller: TextEditingController(),width: size.width * 0.9,),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0),
-              child: TextfielWidget(label: 'Contraseña',controller: TextEditingController(),width: size.width * 0.9,),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Text('Olvidaste tu contraseña?',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
-            ),
-            ButtonWidget(text: 'Iniciar sesion',fontSize: 35,onPressed: () {
-              Navigator.pushNamed(context, LoginScreen.routeName);
-            },),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Text('Registrarse',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
-            ),
-            SvgPicture.asset('assets/layout/fingerprint1.svg'),
-            Text('Usar mi informacion biometrica',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: TextfielWidget(label: 'Telefono',controller: TextEditingController(),width: size.width * 0.9,keyboardType: TextInputType.phone,),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0),
+                child: TextfielWidget(label: 'Contraseña',controller: TextEditingController(),width: size.width * 0.9,obscureText: true,),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: Text('Olvidaste tu contraseña?',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
+              ),
+              ButtonWidget(text: 'Iniciar sesion',fontSize: 35,onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.routeName);
+              },),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40),
+                child: Text('Registrarse',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
+              ),
+              SvgPicture.asset('assets/layout/fingerprint1.svg'),
+              Text('Usar mi informacion biometrica',style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal,color: Color(0xFF818181)),),
+            ],
+          ),
         ),
       ),
     );
